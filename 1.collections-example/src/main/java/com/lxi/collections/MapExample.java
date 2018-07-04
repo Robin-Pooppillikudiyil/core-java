cd9d786ff9a3f9283eaddac58efba41f3956f3a4
Joby Pooppillikudiyil <joby.pooppillikudiyil@nttdata.com>
2018-07-04 22:28:46 +0200

    my initial commit on collections

---
+++ 1.collections-example/src/main/java/com/lxi/collections/MapExample.java
@@ -0,0 +1,42 @@
+package com.lxi.collections;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MapExample {
+
+	private Map<String, User> users = new HashMap<>();
+
+	public static void main(String[] args) {
+		MapExample example = new MapExample();
+		example.createMap();
+		example.printMapOldWay();
+		example.printMapNewWay();
+	}
+
+	private void createMap() {
+		users.put("1", new User("Joby", "Pooppillikudiyil", 37));
+		users.put("2", new User("Jibin", "Pooppillikudiyil", 31));
+		users.put("3", new User("Robin", "Pooppillikudiyil", 24));
+		users.put("4", new User("Thomas", "Pooppillikudiyil", 65));
+		users.put("4", new User("Lizy", "Pooppillikudiyil", 54));
+	}
+
+	private void printMapNewWay() {
+		users.keySet().forEach(key -> printUser(users.get(key)));
+	}
+
+	private void printMapOldWay() {
+
+		for (String key : users.keySet()) {
+			printUser(users.get(key));
+		}
+	}
+
+	private void printUser(User user) {
+		log.info("User is {}", user);
+	}
+}
---
+++ 1.collections-example/src/main/java/com/lxi/collections/User.java
@@ -0,0 +1,19 @@
+package com.lxi.collections;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+	String name;
+	String lastName;
+	Integer age;
+
+}
